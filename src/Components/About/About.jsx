8cefbaecda02779e66fc803595f182861a7e472b
Wesly Ortega <weslyortega@gmail.com>
2022-03-09 00:37:18 -0800

    Added short about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,30 @@
 import './About.scss'
 
+import cap from '../../Assets/Icons/graduation-cap.svg'
+import briefcase from '../../Assets/Icons/briefcase.svg'
+
 function About() {
     return (
         <div className='about-container'>
-            <div className='hello-msg'>Hello! My name is Wesly Ortega</div>    
+            <div className='header'>About</div>
+            <div className='about-item section'>
+                <div className='section-header'>
+                    <div className='section-header-text'>Education</div>
+                    <img className='section-header-icon' src={cap} alt='cap icon' />
+                </div>
+                <div className='section-content'>
+                    B.S. - Computer Science - Cal Poly: San Luis Obispo
+                </div>
+            </div>
+            <div className='about-item section'>
+                <div className='section-header'>
+                    <div className='section-header-text'>Experience</div>
+                    <img className='section-header-icon' src={briefcase} alt='cap icon' />
+                </div>
+                <div className='section-content'>
+                    Software Engineer Intern @ Intuit - Summer 2021
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/Components/HelloMsg/HelloMsg.jsx
@@ -0,0 +1,9 @@
+import './HelloMsg.scss'
+
+function HelloMsg() {
+    return (
+        <div className='about-item hello-msg'>Hello! My name is Wesly Ortega</div>
+    )
+}
+
+export default HelloMsg;
