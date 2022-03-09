2d8cf7164ab5e01346562494c00743c212c70297
Wesly Ortega <weslyortega@gmail.com>
2022-03-08 23:22:40 -0800

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <About/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
