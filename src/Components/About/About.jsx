fcbb50b7066aa0141b7b154fc6a1877b0d6114f5
Wesly Ortega <weslyortega@gmail.com>
2022-03-08 23:22:56 -0800

    Added welcome message

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,11 @@
+import './About.scss'
+
+function About() {
+    return (
+        <div className='about-container'>
+            <div className='hello-msg'>Hello! My name is Wesly Ortega</div>    
+        </div>
+    )
+}
+
+export default About;
