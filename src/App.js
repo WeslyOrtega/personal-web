7d84826d2a5a15db571d2139f74080bdd89c4eea
Wesly Ortega <weslyortega@gmail.com>
2022-03-09 00:42:11 -0800

    Added hello message

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar'
-import About from './Components/About/About';
+import "./App.css";
+
+import Navbar from "./Components/Navbar/Navbar";
+import HelloMsg from "./Components/HelloMsg/HelloMsg";
+import About from "./Components/About/About";
+
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <About/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <HelloMsg />
+            <About />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/HelloMsg/HelloMsg.jsx
+++ src/Components/HelloMsg/HelloMsg.jsx
@@ -2,8 +2,8 @@ import './HelloMsg.scss'
 
 function HelloMsg() {
     return (
-        <div className='about-item hello-msg'>Hello! My name is Wesly Ortega</div>
-    )
+        <div className='hello-msg'>Hello! My name is Wesly Ortega</div>
+    );
 }
 
 export default HelloMsg;
