260649899e8900c0d69c37f6667ef29bcd67f461
Wesly Ortega <weslyortega@gmail.com>
2022-03-09 23:51:58 -0800

    Added typing animation

--- src/Components/HelloMsg/HelloMsg.jsx
+++ src/Components/HelloMsg/HelloMsg.jsx
@@ -1,8 +1,30 @@
 import './HelloMsg.scss'
 
+import { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
+
 function HelloMsg() {
+
+    const element = useRef(null);
+    const typed = useRef(null)
+
+    useEffect(() => {
+        const options = {
+            strings: ["Hello! My name is Wesly Ortega"],
+            typeSpeed: 50,
+        };
+
+        typed.current = new Typed(element.current, options);
+
+        return () => {
+            typed.current.destroy();
+        }
+    }, [])
+
     return (
-        <div className='hello-msg'>Hello! My name is Wesly Ortega</div>
+        <div className='hello-msg'>
+            <span ref={element}></span>    
+        </div>
     );
 }
 
