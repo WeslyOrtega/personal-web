8f03036a67f1129849703374c0cd2ad2542358e4
Wesly Ortega <weslyortega@gmail.com>
2022-03-23 00:32:35 -0700

    created dropdown menu class

---
+++ src/MazeGeneration/Components/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default function DropdownMenu({ label, value, options, onChange }) {
+    return (
+        <div className="dropdown-menu">
+            <label className="dropdown-menu-label">{label}</label>
+            <select className="dropdown-menu-items" value={value} onChange={(e) => onChange(e.target.value)}>
+                {
+                    options.map((option, optionNumber) =>
+                        <option key={optionNumber} value={option.value}>{option.label}</option>
+                    )
+                }
+            </select>
+        </div>
+    );
+}
