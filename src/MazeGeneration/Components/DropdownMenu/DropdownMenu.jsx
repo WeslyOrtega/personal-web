6d82350c60516de33dfbc709853f35decbcb5934
Wesly Ortega <weslyortega@gmail.com>
2022-03-23 22:36:00 -0700

    Added styling to dropdown menu

--- src/MazeGeneration/Components/DropdownMenu/DropdownMenu.jsx
+++ src/MazeGeneration/Components/DropdownMenu/DropdownMenu.jsx
@@ -1,14 +1,17 @@
+import "./DropdownMenu.scss";
+
 export default function DropdownMenu({ label, value, options, onChange }) {
     return (
         <div className="dropdown-menu">
-            <label className="dropdown-menu-label">{label}</label>
-            <select className="dropdown-menu-items" value={value} onChange={(e) => onChange(e.target.value)}>
-                {
-                    options.map((option, optionNumber) =>
-                        <option key={optionNumber} value={option.value}>{option.label}</option>
-                    )
-                }
-            </select>
+            <label className="dropdown-menu-label">{label}
+                <select className="dropdown-menu-items" value={value} onChange={(e) => onChange(e.target.value)}>
+                    {
+                        options.map((option, optionNumber) =>
+                            <option key={optionNumber} value={option.value}>{option.label}</option>
+                        )
+                    }
+                </select>
+            </label>
         </div>
     );
 }
